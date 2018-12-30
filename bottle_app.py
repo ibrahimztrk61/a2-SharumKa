dbd18aa95ae7c745cc87ec28126a931de29acca7
SharumKa <32220897+SharumKa@users.noreply.github.com>
2018-12-30 17:39:39 +0300

    trying

--- bottle_app.py
+++ bottle_app.py
@@ -1,40 +1,70 @@
+from bottle import route, run, error, static_file, template, TEMPLATE_PATH, debug, default_app
+TEMPLATE_PATH.insert(0,'/')
 
-#####################################################################
-### Assignment skeleton
-### You can alter the below code to make your own dynamic website.
-### The landing page for assignment 3 should be at /
-#####################################################################
 
-from bottle import route, run, default_app, debug
+
+@route('/')
+def index():
+    return template('index')
+
+
+def css_callback(filename):
+    return static_file(filename, root='./')
+route('/<filename>','GET', css_callback)
+
+
+def img_callback(filename):
+    return static_file(filename, root='./my_pipe_collection')
+route('/my_pipe_collection/<filename>','GET', img_callback)
+
+
+def img_call(filename):
+    return static_file(filename, root='./my_pipe_collection/Tools')
+route('/my_pipe_collection/Tools/<filename>','GET', img_call)
+
+
+
+@route('/')
+def main_page():
+    return static_file("index.html", root='./')
+
+
+@route('/index.html')
+def characters_page():
+    return static_file("index.html", root='./')
+
+
+@route('/effects.html')
+def characters_page():
+    return static_file("effects.html", root='./')
+
+@route('/observation.html')
+def reviews_page():
+    return static_file("observation.html", root='./')
+
+@route('/tools.html')
+def movies_page():
+    return static_file("tools.html", root='./')
+
 
 def htmlify(title,text):
-    page = """
-        <!doctype html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8" />
-                <title>%s</title>
-            </head>
-            <body>
-            %s
-            </body>
-        </html>
-
-    """ % (title,text)
+    page = '''
+    !DOCTYPE html>
+    <html>
+    <head>
+        <title>%s</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+       %s
+    </body>
+    </html>
+    ''' % (title,text)
     return page
 
-def index():
-    return htmlify("My lovely website",
-                   "This is going to be an awesome website, when it is finished.")
-
-route('/', 'GET', index)
-
-#####################################################################
-### Don't alter the below code.
-### It allows this website to be hosted on Heroku
-### OR run on your computer.
-#####################################################################
-
+@error(404)
+def error404(error):
+    return '<h1> You have experienced a 404<h1>'
 # This line makes bottle give nicer error messages
 debug(True)
 # This line is necessary for running on Heroku
